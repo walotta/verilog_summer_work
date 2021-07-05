11910531af93dd03710c15583ab11a6e7dc32cc8
walotta <walotta@126.com>
2021-07-05 15:08:19 +0800

    init

---
+++ add.v
@@ -0,0 +1,12 @@
+// created by ivy on 2021/6/30
+
+module Add (
+    input [31:0] a,
+    input [31:0] b,
+    output reg [31:0] sum
+);
+    // TO DO
+    // use ~, !, |, & to implement an 32-bits adder
+    // If the digits overflow, discard the overflow digits directly
+
+endmodule
