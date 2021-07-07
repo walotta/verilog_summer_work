b01cc5f94df0863dd8fdb7cc2b24404ef3915fec
walotta <walotta@126.com>
2021-07-07 14:20:36 +0800

    finish adder

--- add.v
+++ add.v
@@ -6,7 +6,22 @@ module Add (
     output reg [31:0] sum
 );
     // TO DO
-    // use ~, !, |, & to implement an 32-bits adder
+    // use ~, ^, |, & to implement an 32-bits adder
     // If the digits overflow, discard the overflow digits directly
 
+    reg add_i;
+    integer i;
+
+    initial begin
+        add_i=0;
+    end
+
+    always @(*) begin
+        
+        for(i=0;i<32;i=i+1)begin
+            sum[i]=add_i^(b[i]^a[i]);
+            add_i=(add_i&a[i])|(a[i]&b[i])|(add_i&b[i]);
+        end
+
+    end
 endmodule
